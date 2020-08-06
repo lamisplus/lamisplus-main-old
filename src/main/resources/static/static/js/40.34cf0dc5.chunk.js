36ec1f28c2f54ec9f88df715a8efe47257d477b1
Deborah Obanisola <ojuolapedaina@gmail.com>
2020-08-06 15:52:33 +0100

    removed build files

--- src/main/resources/static/static/js/40.34cf0dc5.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{2055:function(n,w){}}]);
-//# sourceMappingURL=40.34cf0dc5.chunk.js.map
--- src/main/resources/static/static/js/41.2d3a3c05.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[41],{2053:function(n,w){}}]);
-//# sourceMappingURL=41.2d3a3c05.chunk.js.map
