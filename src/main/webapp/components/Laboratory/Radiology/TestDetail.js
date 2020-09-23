c6f586804e7c237f4aacf3fba5f44bbf7d142d37
Deborah Obanisola <ojuolapedaina@gmail.com>
2020-09-23 18:16:28 +0100

    fixed broken link

--- src/main/webapp/components/Laboratory/Radiology/TestDetail.js
+++ src/main/webapp/components/Laboratory/Radiology/TestDetail.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import {fetchByHospitalNumber} from "actions/patients";
 import {fetchRadiologyTestOrdersByEncounterID, updateRadiologyByFormId} from "actions/laboratory"
 import { TiArrowBack } from 'react-icons/ti';
-import Link from '@material-ui/core/Link';
+import { Link } from 'react-router-dom';
 import Button from "@material-ui/core/Button";
 import {makeStyles} from "@material-ui/core/styles";
 import PatientDetailCard from "../../Functions/PatientDetailCard";
