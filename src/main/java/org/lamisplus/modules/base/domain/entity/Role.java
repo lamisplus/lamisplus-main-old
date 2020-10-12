071e5acf4554f480d1e9ba39496364529bb973cf
Brian Mwasi <mwasi.brian@gmail.com>
2020-10-12 11:54:12 +0300

    Added privileges

---
+++ src/main/java/org/lamisplus/modules/base/domain/entity/Role.java
@@ -0,0 +1,53 @@
+package org.lamisplus.modules.base.domain.entity;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue
+    @Getter
+    @Setter
+    private Long id;
+
+    @NonNull
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    @ManyToMany(cascade = CascadeType.PERSIST)
+    @JoinTable(name = "Permission")
+    private Set<Permission> permissions;
+
+    @Override
+    public int hashCode() {
+        if (id != null) {
+            return id.hashCode();
+        } else if (name != null) {
+            return name.hashCode();
+        }
+
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object another) {
+        if (another == null || !(another instanceof Role))
+            return false;
+
+        Role anotherRole = (Role) another;
+
+        return anotherRole.id != null && (anotherRole.id == this.id);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
