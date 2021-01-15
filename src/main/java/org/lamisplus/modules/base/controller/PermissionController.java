185ee5a830898c7fc05db5598c27f06adf86eec9
Emeka <ilozuechukwuemeka@yahoo.com>
2021-01-15 16:23:12 +0100

    PermissionController modifications

--- src/main/java/org/lamisplus/modules/base/controller/PermissionController.java
+++ src/main/java/org/lamisplus/modules/base/controller/PermissionController.java
@@ -4,9 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.lamisplus.modules.base.domain.entity.Permission;
 import org.lamisplus.modules.base.repository.PermissionRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +16,15 @@ public class PermissionController {
     private final PermissionRepository permissionRepository;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('user_write')")
     public ResponseEntity<List<Permission>> getAll() {
         return ResponseEntity.ok(this.permissionRepository.findAll());
+    }
+
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('user_write')")
+    public ResponseEntity<List<Permission>> save(@RequestBody List<Permission> permissions) {
+        return ResponseEntity.ok(this.permissionRepository.saveAll(permissions));
     }
 }
