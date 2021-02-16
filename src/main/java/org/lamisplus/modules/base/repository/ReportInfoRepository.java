7f2990f0e524cedb0c3ec8d6e2a5d5a51daa3ab1
Emeka <ilozuechukwuemeka@yahoo.com>
2021-02-17 00:00:06 +0100

    Report code modifications

--- src/main/java/org/lamisplus/modules/base/repository/ReportInfoRepository.java
+++ src/main/java/org/lamisplus/modules/base/repository/ReportInfoRepository.java
@@ -4,8 +4,14 @@ import org.lamisplus.modules.base.domain.entity.ReportInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReportInfoRepository extends JpaRepository<ReportInfo, Long>, JpaSpecificationExecutor {
     Optional<ReportInfo> findByName(String name);
+    List<ReportInfo> findAllByArchived(int archived);
+
+    Optional<ReportInfo> findByNameAndArchived(String name, int archived);
+
+    Optional<ReportInfo> findByIdAndArchived(Long id, int archived);
 }
