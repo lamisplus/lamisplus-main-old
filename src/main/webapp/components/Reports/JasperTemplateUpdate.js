b62956463bcd7a40d4089fe6f32e363f9f3a43d8
ojuolarpe <ojuolapedaina@gmail.com>
2021-04-07 14:10:58 +0100

    report formio fix

--- src/main/webapp/components/Reports/JasperTemplateUpdate.js
+++ src/main/webapp/components/Reports/JasperTemplateUpdate.js
@@ -53,6 +53,7 @@ const UpdateReports = (props) => {
             toast.error("Something went wrong, please contact administration");
 
         }
+        formData['resourceObject'] = JSON.parse(res);
         props.update(formData.id, formData, onSuccess, onError)
     }
 
