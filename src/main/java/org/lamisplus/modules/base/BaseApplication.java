0828836fb72a3917de9339ac123f195b72ff25b0
Emeka <ilozuechukwuemeka@yahoo.com>
2021-05-10 15:54:46 +0100

    code update

---
+++ src/main/java/org/lamisplus/modules/base/BaseApplication.java
@@ -0,0 +1,51 @@
+package org.lamisplus.modules.base;
+
+import com.foreach.across.config.AcrossApplication;
+import com.foreach.across.modules.hibernate.jpa.AcrossHibernateJpaModule;
+import com.foreach.across.modules.web.AcrossWebModule;
+import lombok.extern.slf4j.Slf4j;
+import org.lamisplus.modules.base.config.ApplicationProperties;
+import org.lamisplus.modules.bootstrap.BootstrapModule;
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.io.File;
+
+@EnableScheduling
+@AcrossApplication(
+        modules = {
+                AcrossHibernateJpaModule.NAME,
+                AcrossWebModule.NAME, BootstrapModule.NAME, BaseModule.NAME
+        },
+        modulePackageClasses = {BootstrapModule.class})
+@Slf4j
+public class BaseApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+
+
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+        yaml.setResources(new FileSystemResource(ApplicationProperties.modulePath + File.separator +"config.yml"));
+        propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
+        propertySourcesPlaceholderConfigurer.setIgnoreResourceNotFound(true);
+        return propertySourcesPlaceholderConfigurer;
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BaseApplication.class);
+    }
+}
