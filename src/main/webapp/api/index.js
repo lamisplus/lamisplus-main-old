5e658a4b82e6b591b307a59e3d2e2aac0f010096
Mathew <mathewadegbite@gmail.com>
2021-05-21 15:22:25 +0100

    Upload Button for Report Build

--- src/main/webapp/api/index.js
+++ src/main/webapp/api/index.js
@@ -6,7 +6,7 @@
 //export const url =  'http://www.lamisplus.org/base-module/api/';
 
 //Local Server
-//export const url =  '/api/';
+//export const url =  'https://lp-base-app.herokuapp.com/api/';
 
 export const url = 'http://localhost:8484/api/'
 
