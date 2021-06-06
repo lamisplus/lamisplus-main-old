6c445363007562c29bf78d61511294a7aadefa70
Emeka <ilozuechukwuemeka@yahoo.com>
2021-06-06 21:25:31 +0100

    Setting an assigned organisationUnit as currentOrganisationUnit

--- src/main/java/org/lamisplus/modules/base/controller/AccountController.java
+++ src/main/java/org/lamisplus/modules/base/controller/AccountController.java
@@ -3,6 +3,8 @@ package org.lamisplus.modules.base.controller;
 import com.foreach.across.core.annotations.Exposed;
 import org.lamisplus.modules.base.controller.vm.ManagedUserVM;
 import org.lamisplus.modules.base.domain.dto.UserDTO;
+import org.lamisplus.modules.base.domain.entity.ApplicationUserOrganisationUnit;
+import org.lamisplus.modules.base.domain.entity.User;
 import org.lamisplus.modules.base.repository.UserRepository;
 import org.lamisplus.modules.base.service.UserService;
 import org.lamisplus.modules.base.util.PaginationUtil;
@@ -38,10 +40,22 @@ public class AccountController {
 
     @GetMapping("/account")
     public UserDTO getAccount(Principal principal){
-        return userService
+        UserDTO userDTO =  userService
                 .getUserWithRoles()
                 .map(UserDTO::new)
                 .orElseThrow(() -> new AccountResourceException("User could not be found"));
+
+        User user = userRepository.getOne(userDTO.getId());
+
+        if(userDTO.getCurrentOrganisationUnitId() == null && !userDTO.getApplicationUserOrganisationUnits().isEmpty()){
+            for (ApplicationUserOrganisationUnit applicationUserOrganisationUnit : userDTO.getApplicationUserOrganisationUnits()) {
+                user.setCurrentOrganisationUnitId(applicationUserOrganisationUnit.getOrganisationUnitId());
+                userRepository.save(user);
+                break;
+            }
+        }
+
+        return userDTO;
     }
 
     @PostMapping("/register")
