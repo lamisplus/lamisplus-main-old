5967236efb82fe5958625bd024af13b0f89b2995
Emeka <ilozuechukwuemeka@yahoo.com>
2021-07-15 13:19:43 +0100

    code update to ContextProvider

--- src/main/java/org/lamisplus/modules/base/config/ContextProvider.java
+++ src/main/java/org/lamisplus/modules/base/config/ContextProvider.java
@@ -1,20 +1,25 @@
 package org.lamisplus.modules.base.config;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ContextProvider implements ApplicationContextAware {
+    @Autowired
     private static ApplicationContext CONTEXT;
 
-    public ContextProvider() {
-    }
 
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         CONTEXT = applicationContext;
     }
+
+    public ApplicationContext getApplicationContext() {
+        return CONTEXT;
+    }
+
 
     public static <T> T getBean(Class<T> beanClass) {
         return CONTEXT.getBean(beanClass);
