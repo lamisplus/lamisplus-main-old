9894a5b4e4319987035a031e831229df836e2aa7
Mathew <mathewadegbite@gmail.com>
2021-08-13 09:09:53 +0100

    Version update

--- src/main/webapp/components/Layout/Footer.js
+++ src/main/webapp/components/Layout/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
     <Navbar className='mt-3'>
       <Nav navbar >
         <NavItem >
-         <span className='float-right mr-1' style={{fontSize:'14px', fontColor:'#000000', fontWeight:'bolder', position: 'bottom', bottom:'0'}}> Version 1.0</span>
+         <span className='float-right mr-1' style={{fontSize:'14px', fontColor:'#000000', fontWeight:'bolder', position: 'bottom', bottom:'0'}}> Version 1.1</span>
         </NavItem>
       </Nav>
     </Navbar>
