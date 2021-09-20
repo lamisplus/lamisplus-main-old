b3efa192ba87324514239c7854514eb75f4b0474
ojuolarpe <ojuolapedaina@gmail.com>
2021-09-20 09:09:45 +0100

    Auto stash before merge of "debby-update" and "origin/features/emeka"

--- src/main/webapp/api/index.js
+++ src/main/webapp/api/index.js
@@ -6,6 +6,7 @@
 //export const url =  'http://www.lamisplus.org/base-module/api/';
 
 //Local Server
+export const url =  'http://localhost:8082/api/';
 //export const url =  'http://localhost:8080/api/';
 //export const url =  'http://localhost:8080/api/';
 
