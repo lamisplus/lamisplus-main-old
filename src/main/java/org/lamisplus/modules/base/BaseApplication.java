86f6417ce847115e268c2550b7ca05abf9304806
Emeka <ilozuechukwuemeka@yahoo.com>
2022-01-21 11:41:12 +0100

    code update

--- src/main/java/org/lamisplus/modules/base/BaseApplication.java
+++ src/main/java/org/lamisplus/modules/base/BaseApplication.java
@@ -3,21 +3,14 @@ package org.lamisplus.modules.base;
 import com.foreach.across.config.AcrossApplication;
 import com.foreach.across.modules.hibernate.jpa.AcrossHibernateJpaModule;
 import com.foreach.across.modules.web.AcrossWebModule;
-import com.google.common.collect.Lists;
-import liquibase.integration.spring.SpringLiquibase;
 import lombok.extern.slf4j.Slf4j;
-import org.lamisplus.modules.base.config.ApplicationProperties;
 import org.lamisplus.modules.bootstrap.BootstrapModule;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.io.FileSystemResource;
 
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -34,8 +27,6 @@ import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.List;
 
-import java.io.File;
-
 @AcrossApplication(
         modules = {
                 AcrossHibernateJpaModule.NAME,
@@ -48,9 +39,6 @@ import java.io.File;
 @EnableScheduling
 public class BaseApplication extends SpringBootServletInitializer {
 
-    @Autowired
-    private DataSource dataSource;
-
     private static ConfigurableApplicationContext context;
 
     public static void main(String[] args) {
@@ -58,94 +46,9 @@ public class BaseApplication extends SpringBootServletInitializer {
         context = SpringApplication.run(BaseApplication.class, args);
     }
 
-    @Bean
-    //Reads database properties from the config.yml
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
-        yaml.setResources(new FileSystemResource(ApplicationProperties.modulePath + File.separator +"config.yml"));
-        propertySourcesPlaceholderConfigurer.setProperties(yaml.getObject());
-        propertySourcesPlaceholderConfigurer.setIgnoreResourceNotFound(true);
-        return propertySourcesPlaceholderConfigurer;
-    }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(BaseApplication.class);
     }
-
-    /*
-     * Provides sensible defaults and convenience methods for configuration.
-     * @return a Docket
-     */
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(apiInfo())
-                .securityContexts(Arrays.asList(securityContext()))
-                .securitySchemes(Arrays.asList(apiKey()))
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    /*
-     *
-     * @return ApiInfo for documentation
-     */
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Lamisplus")
-                .description("Lamisplus Application Api Documentation")
-                .license("Apache 2.0")
-                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-                .termsOfServiceUrl("http://swagger.io/terms/")
-                .version("1.0.0").contact(new Contact("Development Team","http://lamisplus.org/base-module", "info@lamisplus.org"))
-                .build();
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(defaultAuth()).build();
-    }
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
-    }
-
-    /*
-     * @Param name
-     * @Param keyName
-     * @Param passAs
-     * @return ApiKey
-     * Sending Authorization:
-     */
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", "Authorization", "header");
-    }
-
-    @Bean
-    public SpringLiquibase liquibase() {
-        SpringLiquibase liquibase = new SpringLiquibase();
-        liquibase.setChangeLog("classpath:liquibase/master-changelog-1.0.xml");
-        liquibase.setDataSource(dataSource);
-        return liquibase;
-    }
-
-    /*public static void restart() {
-        ApplicationArguments args = context.getBean(ApplicationArguments.class);
-
-        Thread thread = new Thread(() -> {
-            context.close();
-            context = SpringApplication.run(BaseApplication.class, args.getSourceArgs());
-        });
-
-        thread.setDaemon(false);
-        thread.start();
-    }*/
 }
